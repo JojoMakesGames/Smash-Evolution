85711b67c6bf6c34dea9d63f95ce905d8d26a262
Jojo Oatey <jojo.oatey@gmail.com>
2023-02-21 09:33:20 -0600

    can jump through platforms but land on them

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Trigger");
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        Debug.Log("Trigger Exit");
-    }
+   
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight = 3f;
     public float speed = 1f;
     public bool canJump = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool jumping = false;
 
     // Update is called once per frame
     void Update()
@@ -22,6 +17,16 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Get the player's z velocity
+        float zVelocity = GetComponent<Rigidbody2D>().velocity.y;
+        if (zVelocity > 0 && jumping)
+        {
+            gameObject.layer = (int) Layers.PassTrhoughPlatform;
+        } else
+        {
+            gameObject.layer = (int) Layers.Player;
+        }
+
         // Move the player
         Vector3 move = transform.right * x + transform.up * z;
         transform.position += move * speed * Time.deltaTime;
@@ -29,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
         // Jump
         if (canJump && Input.GetButtonDown("Jump") && Math.Abs(GetComponent<Rigidbody2D>().velocity.y) < 0.001f)
         {
+            jumping = true;
             GetComponent<Rigidbody2D>().AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
         }
 
@@ -44,12 +50,18 @@ public class PlayerMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) 
      {
+        if (jumping) {
+            jumping = false;
+        }
         canJump = true;
      }
  
     void OnCollisionExit2D(Collision2D other) 
     {
-        canJump = false;
+        if (gameObject.layer == (int) Layers.Player)
+        {
+            canJump = false;
+        }
     }
 
 }
