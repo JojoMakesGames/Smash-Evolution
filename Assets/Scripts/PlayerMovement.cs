8fcd1444536c3b33847d6d7817d75929a882c021
Jojo Oatey <jojo.oatey@gmail.com>
2023-02-27 23:09:36 -0600

    add bad wall cling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,15 +16,28 @@ public class PlayerMovement : MonoBehaviour
     public float frictionAmount;
     public int doubleJumps;
 
+    public bool wallCling;
 
-    public float speed = 5f;
     [SerializeField]
     private Rigidbody2D rb;
+
+    public bool isGrounded;
+    public Transform groundCheck;
+    public Transform leftWallCheck;
+    public Transform rightWallCheck;
+    public bool isLeftWall;
+    public bool isRightWall;
+    public float wallRadius = 0.2f;
+    public float groundRadius = 0.2f;
+    public List<LayerMask> whatIsGround;
+    public List<LayerMask> whatIsWall;
+
+    public float speed = 5f;
+    public float wallClingGravity;
 
     private Vector2 moveAmount;
 
     public InputActionAsset actions;
-    public PlayerController controller;
     private InputAction moveAction;
     
 
@@ -54,18 +67,60 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (controller.isGrounded) {
+        foreach(LayerMask ground in whatIsGround)
+        {
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, ground);
+            if (isGrounded)
+            {
+                rb.gravityScale = 1;
+                break;
+            }
+        }
+        #region WallCling
+        foreach(LayerMask wall in whatIsWall)
+        {
+            isLeftWall = Physics2D.OverlapCircle(leftWallCheck.position, wallRadius, wall);
+            isRightWall = Physics2D.OverlapCircle(rightWallCheck.position, wallRadius, wall);
+            if (isLeftWall || isRightWall)
+            {
+                break;
+            }
+        }
+        if (isLeftWall && !isGrounded && moveAmount.x < 0)
+        {
+            rb.AddForce(Vector2.up * rb.velocity.y);
+            rb.gravityScale = wallClingGravity;
+            doubleJumps = 1;
+            wallCling = true;
+        }
+        else if (isRightWall && !isGrounded && moveAmount.x > 0)
+        {
+            rb.AddForce(Vector2.up * rb.velocity.y);
+            rb.gravityScale = .05f;
+            doubleJumps = 1;
+            wallCling = true;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+            wallCling = false;
+        }
+        #endregion
+
+
+        if (isGrounded) {
             doubleJumps = 1;
         }
         float targetSpeed = moveAmount.x * maxSpeed;
         float speedDiff = targetSpeed - rb.velocity.x;
         float movement = Mathf.Pow(Math.Abs(speedDiff) * acceleration, 0.9f) * Mathf.Sign(speedDiff);
-        if (rb.velocity.y < 0) {
+        if (rb.velocity.y < 0 && !wallCling) {
+            gameObject.layer = (int) Layers.Player;
             rb.gravityScale = gravityScaleMultiplier;
         }
         
         rb.AddForce(Vector2.right * movement);
-        if(controller.isGrounded && !moveAction.inProgress){
+        if(isGrounded && !moveAction.inProgress){
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), frictionAmount);
             amount *= Mathf.Sign(rb.velocity.x);
             rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
@@ -84,16 +139,26 @@ public class PlayerMovement : MonoBehaviour
         switch (context.phase)
         {
             case InputActionPhase.Performed:
-                if(controller.isGrounded) {
+                if(isGrounded) {
+                    gameObject.layer = (int) Layers.PassThroughPlatform;
+                    rb.AddForce(Vector2.up * Mathf.Sqrt(maxJumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
+                } else if (wallCling) {
+                    if (isLeftWall) {
+                        rb.AddForce(Vector2.right * Mathf.Sqrt(maxJumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
+                    } else if (isRightWall) {
+                        rb.AddForce(Vector2.left * Mathf.Sqrt(maxJumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
+                    }
                     rb.AddForce(Vector2.up * Mathf.Sqrt(maxJumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
                 } else if (doubleJumps > 0) {
                     rb.velocity = new Vector2(rb.velocity.x, 0);
                     rb.AddForce(Vector2.up * Mathf.Sqrt(maxJumpHeight * -2f * Physics.gravity.y), ForceMode2D.Impulse);
                     doubleJumps--;
-                }
+                } 
                 break;
             case InputActionPhase.Canceled:
-                rb.AddForce(Vector2.down * rb.velocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
+                if (rb.velocity.y > 0) {
+                    rb.AddForce(Vector2.down * rb.velocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
+                }
                 break;
             default:
                 break;
@@ -102,9 +167,9 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnRoll(InputAction.CallbackContext context)
     {
-        // float x = rb.velocity.x;
-        // if(x != 0) {
-        //     rb.AddForce(moveAmount * 10, ForceMode2D.Impulse);
-        // }
+        float x = rb.velocity.x;
+        if(x != 0) {
+            rb.AddForce(moveAmount * 10, ForceMode2D.Impulse);
+        }
     }
 }
